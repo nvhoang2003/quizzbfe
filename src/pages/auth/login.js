05aacc49db7e48ec3543a499b1b590c5e5fd6ad5
anhlan09 <lethianhlan2003@gmail.com>
2023-08-08 11:26:13 +0700

    base

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,150 +1,227 @@
-import { React, useEffect, useState } from 'react';
-import { TextField, Button, FormControl, Box, FormControlLabel, Grid, Link, Typography, Checkbox, InputLabel, OutlinedInput, InputAdornment, IconButton, Stack } from '@mui/material';
-import { loginAuth } from '@/dataProvider/authApi';
-import { setupLocalStorage } from '@/auth/utils';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { styled, alpha } from '@mui/material/styles';
+import { useCallback, useState } from 'react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+  Alert,
+  Box,
+  Button,
+  FormHelperText,
+  Link,
+  Stack,
+  Tab,
+  Tabs,
+  TextField,
+  Typography
+} from '@mui/material';
+import { useAuth } from 'src/hooks/use-auth';
+import { Layout as AuthLayout } from 'src/layouts/auth/layout';
 
-const StyledContent = styled('div')(({ theme }) => ({
-  margin: 'auto',
-  display: 'flex',
-  justifyContent: 'center',
-  backgroundColor: '#fcfcfc',
-  padding: theme.spacing(4, 8),
-  justifyItems: 'center',
-  [theme.breakpoints.up('md')]: {
-    width: 480,
-    flexShrink: 0,
-    padding: theme.spacing(8, 8),
-    boxShadow: '5px 5px 5px 5px #cccccc',
-    border: '1px',
-    borderRadius: '10px'
-  }
-}));
+const Page = () => {
+  const router = useRouter();
+  const auth = useAuth();
+  const [method, setMethod] = useState('email');
+  const formik = useFormik({
+    initialValues: {
+      email: 'demo@devias.io',
+      password: 'Password123!',
+      submit: null
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Password is required')
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        await auth.signIn(values.email, values.password);
+        router.push('/home');
+      } catch (err) {
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    }
+  });
 
-const StyledRoot = styled('main')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100vh',
-  width: '100vw',
-  position: 'relative'
-}));
+  const handleMethodChange = useCallback(
+    (event, value) => {
+      setMethod(value);
+    },
+    []
+  );
 
-export default function login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState({});
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const responseLogin = await loginAuth({ username, password });
-    const { response } = responseLogin;
-    console.log(responseLogin);
-    setupLocalStorage(responseLogin.data.accessToken);
-  };
+  const handleSkip = useCallback(
+    () => {
+      auth.skip();
+      router.push('/home');
+    },
+    [auth, router]
+  );
 
   return (
-    <StyledRoot>
-      <StyledContent >
-        <Stack sx={{ width: 1 }}>
-          <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
-            <Typography variant="h4" align="center">Login</Typography>
-          </Stack>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            display='flex'
-            flexDirection='column'
-          >
-            {errorMessage.mainMessage && errorMessage.mainMessage != "" ? <Alert severity="error">{errorMessage.mainMessage}</Alert> : ''}
-            <FormControl
-              variant="outlined"
+    <>
+      <Head>
+        <title>
+          Login 
+        </title>
+      </Head>
+      <Box
+        sx={{
+          backgroundColor: 'background.paper',
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: '100px',
+            width: '100%'
+          }}
+        >
+          <div>
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
             >
-              <InputLabel htmlFor="username">Username</InputLabel>
-              <OutlinedInput
-                margin="dense"
-                required
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                label="User Name"
-                name="username"
-                autoComplete="username"
-                autoFocus
-              />
-            </FormControl>
-
-            <FormControl
-              variant="outlined"
-              sx={{ mt: 2 }}
-            >
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <OutlinedInput
-                type={showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label={showPassword ? 'show' : 'hide'}
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                margin="dense"
-                required
-                name="password"
-                label="Password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                autoComplete="current-password"
-              />
-            </FormControl>
-
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              variant='contained'
-              size="large"
-              sx={{
-                mt: 1,
-                mb: 2,
-                backgroundColor: 'blue'
-              }}
-            >
-              Sign In
-            </Button>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <Link href="#" variant="body2" underline="hover">
-                  Forgot password?
+              <Typography variant="h4">
+                Login
+              </Typography>
+              <Typography
+                color="text.secondary"
+                variant="body2"
+              >
+                Don&apos;t have an account?
+                &nbsp;
+                <Link
+                  component={NextLink}
+                  href="/auth/register"
+                  underline="hover"
+                  variant="subtitle2"
+                >
+                  Register
                 </Link>
-              </Grid>
-              <Grid item xs={6} display='flex' justifyContent='flex-end'>
-                <Link href="#" variant="body2" underline="hover">
-                  Don't have an account?
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Stack>
-      </StyledContent>
-    </StyledRoot>
-  )
-}
+              </Typography>
+            </Stack>
+            <Tabs
+              onChange={handleMethodChange}
+              sx={{ mb: 3 }}
+              value={method}
+            >
+              <Tab
+                label="Email"
+                value="email"
+              />
+              <Tab
+                label="Phone Number"
+                value="phoneNumber"
+              />
+            </Tabs>
+            {method === 'email' && (
+              <form
+                noValidate
+                onSubmit={formik.handleSubmit}
+              >
+                <Stack spacing={3}>
+                  <TextField
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    fullWidth
+                    helperText={formik.touched.email && formik.errors.email}
+                    label="Email Address"
+                    name="email"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="email"
+                    value={formik.values.email}
+                  />
+                  <TextField
+                    error={!!(formik.touched.password && formik.errors.password)}
+                    fullWidth
+                    helperText={formik.touched.password && formik.errors.password}
+                    label="Password"
+                    name="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    type="password"
+                    value={formik.values.password}
+                  />
+                </Stack>
+                <FormHelperText sx={{ mt: 1 }}>
+                  Optionally you can skip.
+                </FormHelperText>
+                {formik.errors.submit && (
+                  <Typography
+                    color="error"
+                    sx={{ mt: 3 }}
+                    variant="body2"
+                  >
+                    {formik.errors.submit}
+                  </Typography>
+                )}
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  type="submit"
+                  variant="contained"
+                >
+                  Continue
+                </Button>
+                <Button
+                  fullWidth
+                  size="large"
+                  sx={{ mt: 3 }}
+                  onClick={handleSkip}
+                >
+                  Skip authentication
+                </Button>
+                <Alert
+                  color="primary"
+                  severity="info"
+                  sx={{ mt: 3 }}
+                >
+                  <div>
+                    You can use <b>demo@devias.io</b> and password <b>Password123!</b>
+                  </div>
+                </Alert>
+              </form>
+            )}
+            {method === 'phoneNumber' && (
+              <div>
+                <Typography
+                  sx={{ mb: 1 }}
+                  variant="h6"
+                >
+                  Not available in the demo
+                </Typography>
+                <Typography color="text.secondary">
+                  To prevent unnecessary costs we disabled this feature in the demo.
+                </Typography>
+              </div>
+            )}
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => (
+  <AuthLayout>
+    {page}
+  </AuthLayout>
+);
+
+export default Page;
