53528ac680d23fd3fb59e323350ee1d98ee8bcbd
Huwnga <hungbungbu2003@gmail.com>
2023-10-16 16:25:12 +0700

    Hoàng bảo hưng xóa, hưng k bt gì hết

--- src/pages/questionbank/multiChoice/create.js
+++ src/pages/questionbank/multiChoice/create.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import { Card, Divider, Grid} from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import Form from '@/sections/@dashboard/form/questionbank/formMultichoiceQuestion';
 import MultiChoiceForm from '@/sections/@dashboard/form/questionbank/multichoice/form';
 
 // ----------------------------------------------------------------------
