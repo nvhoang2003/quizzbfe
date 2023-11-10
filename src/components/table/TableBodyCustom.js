e769fafcc465acc3c0f9f0556b5f7c7796e16e62
nvhoang2003 <107865780+nvhoang2003@users.noreply.github.com>
2023-11-10 08:52:44 +0700

    add code (#48)

--- src/components/table/TableBodyCustom.js
+++ src/components/table/TableBodyCustom.js
@@ -6,13 +6,13 @@ import { emptyRows } from './utils';
 
 export default function TableBodyCustom ({page, rowsPerPage, dense, listItem, children, notFoundMessage }) {
   const denseHeight = dense ? 52 : 72;
-  const isNotFound = listItem == null || listItem.length == 0;
+  const isNotFound = listItem == null || listItem?.length == 0;
   return (
     <TableBody>
       {children}
       <TableEmptyRows
         height={denseHeight}
-        emptyRows={emptyRows(page, rowsPerPage, listItem.length)}
+        emptyRows={emptyRows(page, rowsPerPage, listItem?.length)}
       />
 
       <TableNoData
